b0c3c820f77d45ca5551c4ba2469e7b50d07641c
David Buggay <drewslifeisgood@yahoo.com>
2021-05-24 00:57:26 -0500

    Level1 (#3)
    * box123
    
    * cylinder
    
    * whatever
    
    * camera controls
    
    camera controls and pan distance
    
    * limit change

---
+++ Assets/CameraController.cs
@@ -0,0 +1,36 @@
+
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float panSpeed = 20f;
+    public float panBorderThickness = 10f;
+    public Vector2 panLimit;
+    // Update is called once per frame
+    void Update() {
+        Vector3 pos = transform.position;
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            pos.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+
+        pos.x = Mathf.Clamp(pos.x, -panLimit.x, panLimit.x);
+        pos.z = Mathf.Clamp(pos.z, -panLimit.y, panLimit.y);
+
+
+        transform.position = pos;
+    }
+}
