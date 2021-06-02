6fa0bd22e7a87387e99a08958f15a38a9ab268b7
David Buggay <drewslifeisgood@yahoo.com>
2021-06-01 23:21:21 -0500

    biggest push ever
    wow

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(BoxCollider))]
+public class BuildingManager : MonoBehaviour
+{
+
+    private BoxCollider _collider;
+    private Building _building = null;
+    private int _nCollisions = 0;
+
+    public void Initialize(Building building)
+    {
+        _collider = GetComponent<BoxCollider>();
+        _building = building;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Ground") return;
+        _nCollisions++;
+        CheckPlacement();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Ground") return;
+        _nCollisions--;
+        CheckPlacement();
+    }
+    public bool CheckPlacement()
+    {
+        if (_building == null) return false;
+        if (_building.IsFixed) return false;
+        bool validPlacement = HasValidPlacement();
+        if (!validPlacement)
+            _building.SetMaterials(BuildingPlacement.INVALID);
+        else
+            _building.SetMaterials(BuildingPlacement.VALID);
+        return validPlacement;
+    }
+    public bool HasValidPlacement()
+    {
+
+
+        if (_nCollisions > 0) return false;
+
+        Vector3 p = transform.position;
+        Vector3 c = _collider.center;
+        Vector3 e = _collider.size / 2f;
+        float bottomHeight = c.y - e.y + 0.5f;
+        Vector3[] bottomCorners = new Vector3[]
+        {
+            new Vector3(c.x - e.x, bottomHeight, c.z - e.z),
+            new Vector3(c.x - e.x, bottomHeight, c.z + e.z),
+            new Vector3(c.x + e.x, bottomHeight, c.z - e.z),
+            new Vector3(c.x + e.x, bottomHeight, c.z + e.z)
+        };
+
+        int invalidCornersCount = 0;
+        foreach (Vector3 corner in bottomCorners)
+        {
+            if (!Physics.Raycast(p + corner,
+                Vector3.up * -1f,
+                2f,
+                Globals.GROUND_LAYER_MASK
+                ))
+                invalidCornersCount++;
+        }
+
+        return invalidCornersCount > 1;
+
+
+    }
+
+       
+}
---
+++ Assets/Scripts/BuildingPlacer.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingPlacer : MonoBehaviour
+{
+    private Building _placedBuilding = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+        
+    }
+    private Ray _ray;
+    private RaycastHit _raycastHit;
+    private Vector3 _lastPlacementPosition;
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKeyDown(KeyCode.B))
+         {
+            _PreparePlacedBuilding(0);
+         }
+        
+        if (_placedBuilding != null)
+        {
+            if(Input.GetKeyUp(KeyCode.Escape))
+            {
+                _CancelPlacedBuilding();
+                return;
+            }
+            _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(
+                _ray,
+                out _raycastHit,
+                1000f,
+                Globals.GROUND_LAYER_MASK
+              ))
+            {
+                _placedBuilding.SetPosition(_raycastHit.point);
+                if(_lastPlacementPosition != _raycastHit.point)
+                {
+                    _placedBuilding.CheckValidPlacement();
+                }
+                _lastPlacementPosition = _raycastHit.point;
+            }
+            if(_placedBuilding.HasValidPlacement && Input.GetMouseButtonDown(0))
+            {
+
+            }
+        }
+        
+        
+    }
+    void _PreparePlacedBuilding(int buildingDataIndex)
+    {
+        if (_placedBuilding != null && !_placedBuilding.IsFixed) 
+            Destroy(_placedBuilding.Transform.gameObject);
+        Building building = new Building(
+            Globals.BUILDING_DATA[buildingDataIndex]
+            );
+        building.Transform.GetComponent<BuildingManager>().Initialize(building);
+        _placedBuilding = building;
+        _lastPlacementPosition = Vector3.zero;
+    }
+    void _PlaceBuilding()
+    {
+        _placedBuilding.Place();
+        _PreparePlacedBuilding(_placedBuilding.DataIndex);
+    }
+    
+    void _CancelPlacedBuilding()
+    {
+        Destroy(_placedBuilding.Transform.gameObject);
+        _placedBuilding = null;
+    }
+}
